c8ad7284407a7490454fcfd8fcb079497282a5f3
Mike <mike@heliotrope.digital>
2023-03-28 13:48:28 +0300

    Added Camera Screen

---
+++ src/screens/camera-screen/camera-screen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from './camera-screen.styles';
+
+function CameraScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Camera Screen</Text>
+    </View>
+  );
+}
+
+export default CameraScreen;
