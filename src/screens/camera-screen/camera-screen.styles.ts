676aedf9c039b68c8ee2c7cf0aedcc4ee375151a
Mike <mike@heliotrope.digital>
2023-04-01 18:23:44 +0300

    Added example screen

--- src/screens/camera-screen/camera-screen.styles.ts
+++ src/screens/camera-screen/camera-screen.styles.ts
@@ -1,13 +1,20 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-  screen: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
+  // screen: {
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   height: '100%',
+  // },
+  // title: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  // },
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: '600',
+  preview: {
+    flex: 1,
   },
 });
--- src/screens/camera-screen/camera-screen.tsx
+++ src/screens/camera-screen/camera-screen.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {View} from 'react-native';
+import {ARKit} from 'react-native-arkit';
+
 import styles from './camera-screen.styles';
 
 function CameraScreen() {
+  const arViewRef = useRef<ARKit>(null);
+  const [node, setNode] = useState<React.ReactElement | null>(null);
+
+  const onARViewCreated = () => {
+    const node = (
+      <ARKit.Box
+        position={{x: 0, y: 0, z: -1}}
+        shape={{width: 0.1, height: 0.1, length: 0.1}}
+        material={{color: '#ff0000'}}
+      />
+    );
+    setNode(node);
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Camera Screen</Text>
+    <View style={styles.container}>
+      <ARKit
+        ref={arViewRef}
+        style={styles.preview}
+        debug
+        onARKitError={(error: any) => console.log(error)}
+        onARViewCreated={onARViewCreated}>
+        {node}
+      </ARKit>
     </View>
   );
 }
